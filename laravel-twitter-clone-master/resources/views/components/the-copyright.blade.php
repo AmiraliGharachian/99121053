ee2ad10ac1aac82edb8cb3564219ed21d1593979
AmiraliGharachian <amiraligharach6@gmail.com>
2024-01-17 22:57:24 +0330

    .

---
+++ laravel-twitter-clone-master/resources/views/components/the-copyright.blade.php
@@ -0,0 +1,5 @@
+<div class="mt-3 mb-0">
+    <small>
+        &copy; {{ \Carbon\Carbon::now()->format('Y') . ' ' . config('app.name') }} - Developed by <a class="text-decoration-none" href="https://jrwebdeveloper.com" target="_blank">Lasha Kajaia</a>
+    </small>
+</div>
